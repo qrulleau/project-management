a5680a36787e5b3b35f949400e4572dfd8f48863
qrulleau <quentin.rulleau@yahoo.com>
2022-10-02 21:37:45 +0200

    ADD: methods for environnement

--- Environment.php
+++ Environment.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once 'Project.php';
+
 class Environment
 {
 	private int $id;
@@ -9,34 +11,73 @@ class Environment
 	private string $ssh_username;
 	private string $phpmyadmin_link;
 	private bool $ip_restriction;
+	private Project $project;
 
-	public function getCode(): string
-	{
-		return $this->code;
-	}
-
-	public function setCode(string $code): void
-	{
-		$this->code = $code;
-	}
 
 	public function getName(): string
 	{
 		return $this->name;
 	}
 
+  public function getID(): int
+	{
+		return $this->id;
+	}
+	public function getProject(): Project
+	{
+		return $this->project;
+	}
+	public function getSSHPort(): int
+	{
+		return $this->ssh_port;
+	}
+	public function getSSHUserName(): string
+	{
+		return $this->ssh_username;
+	}
+	public function getPHPAdminLink(): string
+	{
+		return $this->phpmyadmin_link;
+	}
+	public function getIPRestriction(): bool
+	{
+		return $this->ip_restriction;
+	}
+	public function getIPAdress(): string
+	{
+		return $this->ip_adress;
+	}
+	public function setProject(Project $project): void
+	{
+		$this->project = $project;
+	}
 	public function setName(string $name): void
 	{
 		$this->name = $name;
 	}
-
-    public function getNotes(): string
+	public function setID(int $id): void {
+		$this->id = $id;
+	}
+	public function setIPAdress(int $ip_adress): void {
+		$this->ip_adress = $ip_adress;
+	}
+	public function setSSHName(string $ssh_username): void {
+		$this->ssh_username = $ssh_username;
+	}
+	public function setSSHPort(int $ssh_port): void {
+		$this->ssh_port = $ssh_port;
+	}
+	public function setPHPAdminLink(string $php_admin_link): void
 	{
-		return $this->notes;
+		$this->phpmyadmin_link = $php_admin_link;
 	}
 
-	public function setNotes(string $notes): void
+	public function setIPRestriction(bool $ip_restriction): void
 	{
-		$this->notes = $notes;
+		$this->ip_restriction = $ip_restriction;
 	}
+
+	
+
+	
 }
