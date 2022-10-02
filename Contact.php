d746e24ff3a99cab52681a96e25d6016fa41d4e9
CharlesLLM <charlesllm@orange.fr>
2022-10-02 22:04:46 +0200

    UPDATE: host & contact classes

--- Contact.php
+++ Contact.php
@@ -1,10 +1,26 @@
 <?php
 
+require_once 'Customer.php';
+require_once 'Host.php';
+
 class Contact
 {
+	private int $id;
 	private string $email;
 	private string $phone_number;
 	private string $role;
+	private Customer $customer;
+	private Host $host;
+
+	public function getId(): int 
+	{
+		return $this->id;
+	}
+
+	public function setId(int $id): void
+	{
+		$this->id = $id;
+	}
 
 	public function getEmail(): string
 	{
@@ -34,5 +50,25 @@ class Contact
 	public function setRole(string $role): void
 	{
 		$this->role = $role;
+	}
+
+	public function getCustomer(): Customer
+	{
+		return $this->customer;
+	}
+
+	public function setCustomer (Customer $customer): void
+	{
+		$this->customer = $customer;
+	}
+
+	public function getHost(): Host 
+	{
+		return $this->host;
+	}
+
+	public function setHost (Host $host): void
+	{
+		$this->host = $host;
 	}
 }
