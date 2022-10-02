c8f02f128bf3875e59c22921c9079cf4ca52152a
qrulleau <quentin.rulleau@yahoo.com>
2022-10-02 22:08:29 +0200

    ADD: foreign key

--- Contact.php
+++ Contact.php
@@ -1,10 +1,15 @@
 <?php
 
+require_once 'Host.php';
+require_once 'Customer.php';
+
 class Contact
 {
 	private string $email;
 	private string $phone_number;
 	private string $role;
+	private Host $host_id;
+	private Customer $customer_id;
 
 	public function getEmail(): string
 	{
@@ -34,5 +39,21 @@ class Contact
 	public function setRole(string $role): void
 	{
 		$this->role = $role;
+	}
+	public function getHostId(): Host
+	{
+		return $this->host_id;
+	}
+	public function setHostId(Host $host_id): void
+	{
+		$this->host_id = $host_id;
+	}
+	public function getCustomerId(): Customer
+	{
+		return $this->customer_id;
+	}
+	public function setCustomerId(Customer $customer_id): void
+	{
+		$this->customer_id = $customer_id;
 	}
 }
