cbd2abd3b1d5d450660746c726f242fb99284c9b
qrulleau <quentin.rulleau@yahoo.com>
2022-10-10 16:57:12 +0200

    MIN: update component

--- component/header.php
+++ component/header.php
@@ -1,6 +1,6 @@
 <header>
     <div class="d-flex justify-start">
-        <img src="assets/logo-mentalworks-blanc-trimmy.png" alt="">
+        <img src="http://localhost/project-management/assets/logo-mentalworks-blanc-trimmy.png" alt="">
         <button class="hamburger hamburger--collapse" type="button">
             <span class="hamburger-box">
                 <span class="hamburger-inner"></span>
--- component/nav.php
+++ component/nav.php
@@ -1,8 +1,20 @@
 <div class="background">
     <div class="d-flex direction-column align-start nav-bar">
-        <a href="#">Tableau de bord</a>
-        <a href="#">Projets</a>
-        <a href="#">Client</a>
-        <a href="#">Hebergeur</a>
+        <a href="#" class="d-flex justify-start icon">
+            <i data-feather="home"></i>
+            <p class="nav-text">Tableau de bord</p>
+        </a>
+        <a href="#" class="d-flex justify-start icon">
+            <i data-feather="folder"></i>
+            <p class="nav-text">Projets</p>
+        </a>
+        <a href="#" class="d-flex justify-start icon">
+            <i data-feather="user"></i>
+            <p class="nav-text">Client</p>
+        </a>
+        <a href="#" class="d-flex justify-start icon">
+            <i data-feather="cloud"></i>
+            <p class="nav-text">Hebergeur</p>
+        </a>
     </div>
 </div>
